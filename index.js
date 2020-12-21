8c9f9a0254a6874fffb1b08f793c6729c623d329
812jay <yongje812@gmail.com>
2020-12-22 01:35:47 +0900

    MongoDB Model&Schema

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 mongoose
-  .connect(
-    "mongodb+srv://boilerplateadmin:qwer12%23%24@boilerplate.uvlp5.mongodb.net/boilerplate?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("mongodb connected..."))
   .catch((err) => console.log(err));
 
