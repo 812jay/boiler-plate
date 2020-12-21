0afb30be411a82ea77ccb5d979b5237bc3ccfe32
812jay <yongje812@gmail.com>
2020-12-22 00:56:28 +0900

    connect mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://boilerplateadmin:qwer12%23%24@boilerplate.uvlp5.mongodb.net/boilerplate?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("mongodb connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
