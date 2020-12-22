0c4610beb01e5616ea0629d2ffcb96f0248acf2a
812jay <yongje812@gmail.com>
2020-12-22 17:40:46 +0900

    BodyParser & PostMan & Join function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const { User } = require("./models/User");
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -20,6 +27,18 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  /*회원가입시 필요한 정보들을 client에서 가져오면
+    그 정보들을 DB에 넣어준다.*/
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
